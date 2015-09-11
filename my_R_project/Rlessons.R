991201bd833214a49eb692586275bf803f922d43
nwvidal <nwvidal@gmail.com>
2015-09-11 12:48:43 -0400

    add R project files

---
+++ my_R_project/Rlessons.R
@@ -0,0 +1,43 @@
+# basic operations
+1+100
+x <- 101
+x
+sin(1)
+y <- 102
+x == y
+x > y
+x < y
+x != y
+z <- c(1:10)
+z
+z_nih_scw_sep <- c(1:10)
+rm(z_nih_scw_sep)
+rm(list = ls())
+ls()
+getwd()
+setwd("/home/newton/Desktop/vidal/lectures/software_carpentry_workshop")
+# See installed packages and install new packages
+installed.packages()
+install.packages("dplyr")
+install.packages("gapminder")
+install.packages("ggplot2")
+# override variables
+x <- 3E-4
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+# Chalenge 1 from http://swcarpentry.github.io/r-novice-gapminder/02-project-intro.html
+
+
+
+
+
+
+
+
+
+
+
+
+
